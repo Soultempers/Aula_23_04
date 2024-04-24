b0dbf045a789ca642d5b9ea9d4f1513b77d7ab32
pabllo <pablo.silvacavalcante@hotmail.com>
2024-04-23 22:47:02 -0400

    Initial commit

---
+++ app/src/main/java/com/example/aula_23_04/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.aula_23_04;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Spinner;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinnercurso = findViewById(R.id.spinner_curso);
+        spinnercurso.setPrompt("Escolha o Curso");
+
+        Spinner spinneruniversidade = findViewById(R.id.spinner_universidade);
+        spinneruniversidade.setPrompt("Escolha a universidade");
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
